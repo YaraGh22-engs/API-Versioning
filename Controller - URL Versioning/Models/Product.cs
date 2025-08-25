37d14ce1ba0adbc3932c3770ac4f06e23a400e0f
YaraGh <yaraesmaiel123@gmail.com>
2025-08-25 11:42:40 -0500

    Prepearing files - V1 - V2

---
+++ Controller - URL Versioning/Models/Product.cs
@@ -0,0 +1,8 @@
+namespace Controller___URL_Versioning.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ Controller - URL Versioning/Responses/V2/PriceResponse.cs
@@ -0,0 +1,7 @@
+namespace Controller___URL_Versioning.Responses.V2;
+
+public sealed class PriceResponse
+{
+    public decimal Amount { get; set; }
+    public string Currency { get; set; } = null!;
+}
